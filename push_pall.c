e41124146a0f1af20682dd396b689b912d4bc7f0
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-03 14:05:29 +0300

    monty

--- push_pall.c
+++ push_pall.c
@@ -1,7 +1,13 @@
 #include"monty.h"
+/**
+ * push - push a data to the top of the stack
+ * @top: the top of the stack
+ * @line_number: the number of the line
+ */
 void push(stack_t **top, unsigned int line_number)
 {
 	int data;
+
 	data = atoi(strtok(NULL, " \n\t\r"));
 	if (isInteger(data) == false && *top == NULL)
 	{
@@ -14,9 +20,15 @@ void push(stack_t **top, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+/**
+ * pall - prints all of the data from the stack
+ * @top: the top of the stack
+ * @line_number: the number of the line
+ */
 void pall(stack_t **top, unsigned int line_number)
 {
 	stack_t *tmp = *top;
+
 	UNUSED(line_number);
 	if (*top == NULL)
 	{
